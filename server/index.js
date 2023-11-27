a59199f3d5cab132120115ad07667ed73dfc201e
dannyfrelink <danny-frelink@hotmail.com>
2023-11-27 10:45:57 +0100

    feat: create fetch request to Weather API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello from NodeJS server!");
+app.get("/weather/:region", async (req, res) => {
+  try {
+    const { region } = req.params;
+    const key = process.env.API_KEY;
+
+    await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${key}&q=${region}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log("testing: ", data));
+
+    res.send(`Hello from NodeJS server: ${region}`);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
