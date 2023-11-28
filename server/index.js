6b0277d0ec7682adf1db009cdbcd749c9c07562e
dannyfrelink <danny-frelink@hotmail.com>
2023-11-28 12:37:47 +0100

    feat: create route with fetching input suggestions

--- server/index.js
+++ server/index.js
@@ -7,10 +7,25 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.get("/suggestions", async (req, res) => {
+  try {
+    const { input } = req.query;
+
+    await fetch(
+      `http://api.geonames.org/searchJSON?name_startsWith=${input}&username=dannyfrelink&maxRows=1000`
+    )
+      .then((res) => res.json())
+      .then(({ geonames }) => res.json({ suggestions: geonames }));
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.get("/weather/:region", async (req, res) => {
   try {
     const { region } = req.params;
-    const key = process.env.API_KEY;
+    const key = process.env.WEATHER_API_KEY;
 
     const forecastWeather = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${region}&days=7`
